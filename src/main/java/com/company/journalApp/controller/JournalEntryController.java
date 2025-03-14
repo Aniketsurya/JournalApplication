fb3a4481105431b6ac59fea36bf87f2f660fadca
Aniket Suryavanshi <aniketsuryavanshi4954@gmail.com>
2025-03-14 11:31:38 +0530

    testing all mapping methods with pathvariable and requestbody

--- src/main/java/com/company/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/company/journalApp/controller/JournalEntryController.java
@@ -1,4 +1,43 @@
 package com.company.journalApp.controller;
 
+import com.company.journalApp.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    // Always controller class will be having public access as it should be accessed by spring
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping()
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateJournalEntryById(@PathVariable Long myId,@RequestBody JournalEntry myEntry){
+        return journalEntries.put(myId, myEntry);
+    }
 }
