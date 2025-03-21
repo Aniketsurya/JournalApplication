91f67a3db1ad6ec32c9aa1fed3e3db2d59332484
Aniket Suryavanshi <aniketsuryavanshi4954@gmail.com>
2025-03-21 11:10:09 +0530

    connecting to mongodb

--- src/main/java/com/company/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/company/journalApp/controller/JournalEntryControllerV2.java
@@ -9,35 +9,34 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/_journal")
-public class JournalEntryController {
+@RequestMapping("/journal")
+public class JournalEntryControllerV2 {
 
-    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
 
     // Always controller class will be having public access as it should be accessed by spring
     @GetMapping
     public List<JournalEntry> getAll(){
-        return new ArrayList<>(journalEntries.values());
+
+        return null;
     }
 
     @PostMapping()
     public boolean createEntry(@RequestBody JournalEntry myEntry){
-        journalEntries.put(myEntry.getId(), myEntry);
         return true;
     }
 
     @GetMapping("id/{myId}")
     public JournalEntry getJournalEntryById(@PathVariable Long myId){
-        return journalEntries.get(myId);
+        return null;
     }
 
     @DeleteMapping("id/{myId}")
     public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
-        return journalEntries.remove(myId);
+        return null;
     }
 
     @PutMapping("id/{myId}")
     public JournalEntry updateJournalEntryById(@PathVariable Long myId,@RequestBody JournalEntry myEntry){
-        return journalEntries.put(myId, myEntry);
+        return null;
     }
 }
